efc39a850737557f334adcf42db3c9f369bfe4c5
ibrand <me@ilonabrand.com>
2015-09-03 14:37:33 -0400

    tests reflect the switch from strings to buffers

--- testBencode.js
+++ testBencode.js
@@ -1,37 +1,33 @@
 var bencode = require('./bencode.js');
 
 function testParse(){
-    var fs = require('fs');
+    var toDecode = new Buffer(12);
+    toDecode.write('d4:spam2:hie');
     var decodedFile = {};
-
-    // read file in as a buffer
-    fs.readFile('testFile.torrent', function(err, buffer){
-        if (err) {
-            throw new Error('not a valid file!!!');
-        }
-        console.log('about to decode');
-        // pass the byte array into bencode's parse for decoding
-        decodedFile = bencode.parse(buffer).result;
-        console.log(typeof decodedFile);
-        console.log('decodedFile',decodedFile);
-    });
+    decodedFile = bencode.parse(toDecode);
+    console.log(decodedFile);
 }
 
 function testParseInteger() {
+    var toDecode = new Buffer(9);
     var integerInput = 'i5783247e';
+    toDecode.write(integerInput);
     var correctResult = 5783247;
-    var resultFromTest = bencode.parseInteger(integerInput).result;
+    var resultFromTest = bencode.parseInteger(toDecode).result;
     if (correctResult !== resultFromTest){
         throw new Error('Wrong Integer!!!');
     }
 }
 
 function testParseByteString() {
+    var toDecode = new Buffer(16);
     var byteStringInput = '5:abcdefghijZZZZ';
-    var correctResult = 'abcde';
-    var resultFromTest = bencode.parseByteString(byteStringInput).result;
-    if (correctResult !== resultFromTest){
-        throw new Error('Wrong String!!!');
+    toDecode.write(byteStringInput);
+    var correctResult = new Buffer(5);
+    correctResult.write('abcde');
+    var resultFromTest = bencode.parseByteString(toDecode).result;
+    if (correctResult.compare(resultFromTest)){
+        throw new Error('Wrong Buffer!!!');
     }
 }
 
