7081a8ff1350f85086b6554dd6c0ec33bb98883b
ibrand <me@ilonabrand.com>
2015-09-16 17:25:34 -0400

    strip apart peer's handshake response

--- Peer.js
+++ Peer.js
@@ -11,15 +11,29 @@ Tracker.makeRequestToTracker(function (peerObject){
         console.log('connected to server');
         handshake(client);
     });
-    client.on('data', function(data){
-        console.log(data.toString());
-        client.end();
-    });
+
+    processHandshake(client);
+
+    // client.on('data', function(data){
+    //     console.log('LENGTH in bytes', data.length);
+    //     console.log(data.toString(),'\n');
+    //     client.end();
+    // });
     client.on('end', function(){
         console.log('disconnected from server');
     });
     // perform a handshake with that peer
 });
+
+function readChunk(client, lengthToRead, acquireBuffer){
+    var buffer = client.read(lengthToRead);
+    if (buffer){
+        return acquireBuffer(buffer);
+    }
+    client.once('readable', function(){
+        readChunk(client, lengthToRead, acquireBuffer);
+    });
+}
 
 function handshake(client){
     client.write(new Buffer([19]));
@@ -27,4 +41,25 @@ function handshake(client){
     client.write(new Buffer(8).fill(0));
     client.write(Tracker.getRequestParams().info_hash);
     client.write(Tracker.getRequestParams().peer_id,'utf8');
+}
+
+function processHandshake(client){
+    console.log('IN PROCESS HANDSHAKE');
+    readChunk(client, 1, function(buffer){
+        console.log('Inside first read chunk');
+        console.log('single byte',buffer.toString());
+        readChunk(client, 19, function(buffer){
+            console.log('protocol',buffer.toString());
+            readChunk(client, 8, function(buffer){
+                console.log('reserve bytes',buffer.toString());
+                readChunk(client, 20, function(buffer){
+                    console.log('sha1',buffer.toString());
+                    readChunk(client, 20, function(buffer){
+                        console.log('peer_id',buffer.toString());
+                        client.end();
+                    });
+                });
+             });
+        });
+    });
 }
