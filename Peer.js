7998c44eabc4e7b127e3936c1c2ee86bfe8c85c4
ibrand <me@ilonabrand.com>
2015-09-21 15:05:56 -0400

    created a peer object to keep track of states and outlined if cases for processing incoming messages

--- Peer.js
+++ Peer.js
@@ -2,10 +2,22 @@ var Tracker = require('./Tracker');
 var net = require('net');
 var async = require('async');
 
-Tracker.makeRequestToTracker(function (peerObject){
+Tracker.makeRequestToTracker(function (peerListObject){
     // peer object contains IP addresses as keys and ports as values
     var hostIp = '96.126.104.219'; // Tom's IP will stay constant because he is running the test tracker
-    var port = peerObject[hostIp];
+    var port = peerListObject[hostIp];
+
+    // initialize the states of the peer
+    var peerObject = {
+        hostIp: hostIp,
+        port: port,
+        am_choking: 1,
+        peer_choking: 1,
+        am_interested: 0,
+        peer_interested: 0
+    };
+
+    console.log('peerObject', peerObject);
 
     // open up a socket with the first peer in the obj
     var client = net.connect(port, hostIp, function(){
@@ -19,10 +31,9 @@ Tracker.makeRequestToTracker(function (peerObject){
         // We have finished processing the handshake
         // 4 how many bytes are in the length header 
         readChunk(client, 4, function(error, buffer){
+            console.log('the length flag', buffer);
             var lengthHeader = buffer.readUIntBE(0,buffer.length);
-            readChunk(client, lengthHeader, function(error, buffer){
-                console.log('rest of message', buffer);
-            });
+            processMessage(peerObject, client, lengthHeader);
         });
     });
 
@@ -72,3 +83,106 @@ function processHandshake(client, finishedHandshake){
     ], finishedHandshake);
 }
 
+function processMessage(peerObject, client, lengthHeader){
+    // messages will be formatted as: <lengthHeader><id><payload>
+    // lengthHeader tells how long the message will be
+    // id tells what type of message you're dealing with
+    // payload is the rest of the body of the message
+
+    if (lengthHeader === 0){
+        // then the msg is keepalive so keep connection open
+        // no id
+        // no payload
+        console.log('in keepalive');
+    }
+    // read one more bit to determine the id of the message
+    readChunk(client, 1, function(error, buffer){
+        var id = buffer.readUIntBE(0,buffer.length);
+        console.log('read chunk');
+        if (id === 0){
+            // choke
+            choke(peerObject, client, lengthHeader);
+        }
+        if (id === 1){
+            // unchoke
+            unchoke(peerObject, client, lengthHeader);
+        }
+        if (id === 2){
+            // interested
+            interested(peerObject, client, lengthHeader);
+        }
+        if (id === 3){
+            // uninterested
+            uninterested(peerObject, client, lengthHeader);
+        }
+        if (id === 4){
+            // have
+            have(peerObject, client, lengthHeader);
+        }
+        if (id === 5){
+            // bitfield
+            bitfield(peerObject, client, lengthHeader);
+        }
+        if (id === 6){
+            // request
+            request(peerObject, client, lengthHeader);
+        }
+        if (id === 7){
+            // piece
+            piece(peerObject, client, lengthHeader);
+        }
+        if (id === 8){
+            // cancel
+            cancel(peerObject, client, lengthHeader);
+        }
+        if (id === 9){
+            // port
+            port(peerObject, client, lengthHeader);
+        }
+        console.log('id',id);
+    });
+}
+
+function choke(peerObject, client, lengthHeader){
+    peerObject.peer_choking = 1;
+    console.log('in choke',peerObject);
+}
+
+function unchoke(peerObject, client, lengthHeader){
+    peerObject.peer_choking = 0;
+    console.log('in unchoke',peerObject);
+}
+
+function interested(peerObject, client, lengthHeader){
+    peerObject.peer_interested = 1;
+    console.log('in interested',peerObject);
+}
+
+function uninterested(peerObject, client, lengthHeader){
+    peerObject.peer_interested = 0;
+    console.log('in uninterested',peerObject);
+}
+
+function have(peerObject, client, lengthHeader){
+    console.log('in have',peerObject);
+}
+
+function bitfield(peerObject, client, lengthHeader){
+    console.log('in bitfield',peerObject);
+}
+
+function request(peerObject, client, lengthHeader){
+    console.log('in request',peerObject);
+}
+
+function piece(peerObject, client, lengthHeader){
+    console.log('in piece',peerObject);
+}
+
+function cancel(peerObject, client, lengthHeader){
+    console.log('in cancel',peerObject);
+}
+
+function port(peerObject, client, lengthHeader){
+    console.log('in port',peerObject);
+}
